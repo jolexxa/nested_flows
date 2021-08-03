c9c88bd41eefb83d9e26b8b0a2a26687e3332ea9
Joanna May <joannamadisonmay@gmail.com>
2021-08-03 11:45:25 -0500

    Update bank link flow

--- lib/bank_link/bank_link_flow.dart
+++ lib/bank_link/bank_link_flow.dart
@@ -3,23 +3,18 @@ import 'package:flutter/material.dart';
 import 'package:nested_flows/bank_link/bank_link_flow_state.dart';
 
 class BankLinkFlow extends StatelessWidget {
-  const BankLinkFlow({Key? key, required this.controller}) : super(key: key);
+  const BankLinkFlow({Key? key}) : super(key: key);
 
-  static Route<BankLinkFlowState> route(
-      {required FlowController<BankLinkFlowState> controller}) {
+  static Route<BankLinkFlowState> route() {
     return MaterialPageRoute(
-      builder: (_) => BankLinkFlow(
-        controller: controller,
-      ),
+      builder: (_) => const BankLinkFlow(),
     );
   }
-
-  final FlowController<BankLinkFlowState> controller;
 
   @override
   Widget build(BuildContext context) {
     return FlowBuilder<BankLinkFlowState>(
-      controller: controller,
+      state: BankLinkFlowState(),
       onGeneratePages: onGeneratePages,
     );
   }
