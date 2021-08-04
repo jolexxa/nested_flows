8ed5fdd61fea0d0eca74e135efa7f58a78346fc8
Joanna May <joannamadisonmay@gmail.com>
2021-08-04 11:55:08 -0500

    Add comments

--- lib/account_selection/account_selection_page.dart
+++ lib/account_selection/account_selection_page.dart
@@ -31,19 +31,40 @@ class AccountSelectionPageView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final l10n = context.l10n;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(l10n.accountSelectionAppBarTitle),
-      ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          return ListTile(
-            leading: const Icon(Icons.account_balance, size: 36),
-            title: Text(accounts[index].name),
-            trailing: const Icon(Icons.chevron_right, size: 36),
-          );
-        },
-        itemCount: accounts.length,
+    return WillPopScope(
+      onWillPop: () async {
+        // Currently causes a rebuild which cancels animations.
+        //
+        // Possibly need support from flow_builder possibly to update the
+        // flow state without updating the pages since it's about to occur
+        // anyways?
+        //
+        // import 'package:flow_builder/flow_builder.dart';
+        // import 'package:nested_flows/bank_link/bank_link.dart';
+        //
+        // context.flow<BankLinkFlowState>().update(
+        //       (flowState) => BankLinkFlowState(
+        //         banks: flowState.banks,
+        //         accounts: flowState.accounts,
+        //         // Clear out other properties
+        //       ),
+        //     );
+        return true;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(l10n.accountSelectionAppBarTitle),
+        ),
+        body: ListView.builder(
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: const Icon(Icons.account_balance, size: 36),
+              title: Text(accounts[index].name),
+              trailing: const Icon(Icons.chevron_right, size: 36),
+            );
+          },
+          itemCount: accounts.length,
+        ),
       ),
     );
   }
